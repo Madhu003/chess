8a214e0488d19145a35c142549c603c141344e21
Madhu003 <madhusudanarya003@gmail.com>
2023-07-11 18:31:36 +0530

    shadow added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,36 @@
 import { useEffect, useState } from "react";
 import constatns from "./constants";
 
-const fullMatrix: any = new Array(8).fill("").map(() => new Array(8).fill(""));
+const fullMatrix: any = new Array(8).fill("").map(() => new Array(8).fill({}));
 
 export default function Home() {
   const [matrix, setMatrix] = useState(fullMatrix);
 
   useEffect(() => {
-    matrix[0] = [...constatns.STARTINGPOSITIONS.BLACK];
-    matrix[1] = matrix[1].map(() => constatns.BLACK.PAWN);
+    matrix[0] = [
+      ...constatns.STARTINGPOSITIONS.BLACK.map((token: string) => ({ token })),
+    ];
+    matrix[1] = matrix[1].map(() => ({ token: constatns.BLACK.PAWN }));
 
-    matrix[6] = matrix[6].map(() => constatns.WHITE.PAWN);
-    matrix[7] = [...constatns.STARTINGPOSITIONS.WHITE];
+    matrix[6] = matrix[6].map(() => ({ token: constatns.WHITE.PAWN }));
+    matrix[7] = [
+      ...constatns.STARTINGPOSITIONS.WHITE.map((token: string) => ({ token })),
+    ];
+
     console.log(matrix);
     setMatrix([...matrix]);
   }, []);
+
+  const cellClickHandler = (i: number, j: number) => {
+    matrix.forEach((row: any) => {
+      row.forEach((cell: any) => {
+        cell.selected = false;
+      });
+    });
+
+    matrix[i][j].selected = true;
+    setMatrix([...matrix]);
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -27,13 +43,14 @@ export default function Home() {
                 <td
                   key={j}
                   style={{ fontSize: "44px", fontWeight: "600" }}
-                  className={`border border-slate-300 relative p-12 ${
+                  className={`border border-slate-300 cursor-pointer p-12 relative shadow-inner  ${
                     (i + j) % 2 == 0 ? "bg-slate-white" : "bg-slate-500"
-                  }`}
+                  } ${cell.selected && "selected-cell"}`}
+                  onClick={() => cellClickHandler(i, j)}
                 >
                   <p
                     className="m-0 p-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                    dangerouslySetInnerHTML={{ __html: cell }}
+                    dangerouslySetInnerHTML={{ __html: cell.token || "" }}
                   ></p>
                 </td>
               ))}
