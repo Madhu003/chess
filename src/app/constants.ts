b811ea139d100b25e61d3577edebdab87c268492
Madhu003 <madhusudanarya003@gmail.com>
2023-07-11 16:05:05 +0530

    player token added

---
+++ src/app/constants.ts
@@ -0,0 +1,42 @@
+const constatns: any = {
+  WHITE: {
+    KING: "&#9812;",
+    ROOK: "&#9814;",
+    KNIGHT: "&#9816;",
+    QUEEN: "&#9813;",
+    BISHOP: "&#9815;",
+    PAWN: "&#9817;",
+  },
+  BLACK: {
+    KING: "&#9818;",
+    ROOK: "&#9820;",
+    KNIGHT: "&#9822;",
+    QUEEN: "&#9819;",
+    BISHOP: "&#9821;",
+    PAWN: "&#9823;",
+  },
+};
+constatns.STARTINGPOSITIONS = {
+  WHITE: [
+    constatns.WHITE.ROOK,
+    constatns.WHITE.KNIGHT,
+    constatns.WHITE.BISHOP,
+    constatns.WHITE.QUEEN,
+    constatns.WHITE.KING,
+    constatns.WHITE.BISHOP,
+    constatns.WHITE.KNIGHT,
+    constatns.WHITE.ROOK,
+  ],
+  BLACK: [
+    constatns.BLACK.ROOK,
+    constatns.BLACK.KNIGHT,
+    constatns.BLACK.BISHOP,
+    constatns.BLACK.KING,
+    constatns.BLACK.QUEEN,
+    constatns.BLACK.BISHOP,
+    constatns.BLACK.KNIGHT,
+    constatns.BLACK.ROOK,
+  ],
+};
+
+export default constatns;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,44 @@
-import Image from "next/image";
-import { useState } from "react";
+"use client";
+import { useEffect, useState } from "react";
+import constatns from "./constants";
 
-const fullMatrix: any = new Array(8).fill("").map(() => new Array(8).fill({}));
+const fullMatrix: any = new Array(8).fill("").map(() => new Array(8).fill(""));
 
 export default function Home() {
   const [matrix, setMatrix] = useState(fullMatrix);
-  
+
+  useEffect(() => {
+    matrix[0] = [...constatns.STARTINGPOSITIONS.BLACK];
+    matrix[1] = matrix[1].map(() => constatns.BLACK.PAWN);
+
+    matrix[6] = matrix[6].map(() => constatns.WHITE.PAWN);
+    matrix[7] = [...constatns.STARTINGPOSITIONS.WHITE];
+    console.log(matrix);
+    setMatrix([...matrix]);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <table>
+      <table className="border-collapse border border-slate-400">
         <tbody>
-          <tr>
-            <td></td>
-          </tr>
+          {matrix.map((row: any, i: number) => (
+            <tr key={i}>
+              {row.map((cell: any, j: number) => (
+                <td
+                  key={j}
+                  style={{ fontSize: "44px", fontWeight: "600" }}
+                  className={`border border-slate-300 relative p-12 ${
+                    (i + j) % 2 == 0 ? "bg-slate-white" : "bg-slate-500"
+                  }`}
+                >
+                  <p
+                    className="m-0 p-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                    dangerouslySetInnerHTML={{ __html: cell }}
+                  ></p>
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
